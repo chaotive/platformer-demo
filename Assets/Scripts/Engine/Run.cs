1fb452c84ec40a865668f082c8bc2968064e86bb
chaotive <leonardo.montenegro@chaotive.cl>
2017-08-08 15:08:18 -0400

    Adding few basic things

---
+++ Assets/Scripts/Engine/Run.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Run : MonoBehaviour {
+    public float speed;
+    public GameObject player;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        player.transform.Translate(Vector2.right * speed * Time.deltaTime);
+	}
+}
