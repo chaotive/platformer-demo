e579f55d942de34f4cb94cdd506ae937f5565c26
Leonardo Montenegro <leonardo.montenegro@chaotive.cl>
2017-08-09 00:58:23 -0400

    Adding more assets re-factoring files making movement physics based

---
+++ Assets/Platformer Assets/Scripts/Collisions/StageLimit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageLimit : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Colission! " + collision);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Trigger! " + collision);
+    }
+
+}
---
+++ Assets/Platformer Assets/Scripts/Player/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public int collectedItems = 0;
+    public int hp;    
+    public float speed;
+
+    private Rigidbody2D body;
+    
+    void Start () {
+        hp = Config.game.playerHp;        
+        speed = Config.game.playerSpeed;
+
+        body = GetComponent<Rigidbody2D>();        
+    }
+
+    /*void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);        
+    }*/
+
+    void FixedUpdate()
+    {
+        body.AddForce(Vector2.right * speed * Time.deltaTime);
+    }
+}
