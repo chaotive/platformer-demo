a85e096060fc8a46e63471929ec7a14d1fc9bf60
Leonardo Montenegro <leonardo.montenegro@chaotive.cl>
2017-08-10 00:14:31 -0400

    Basic jump

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,24 +6,29 @@ public class Player : MonoBehaviour {
     public int collectedItems = 0;
     public int hp;    
     public float speed;
+    public float jumpForce = 1000;
+    public float maxVelocity = 40;
 
     private Rigidbody2D body;
-    
+    private bool jumping;
+
     void Start () {
         hp = Config.game.playerHp;        
         speed = Config.game.playerSpeed;
 
-        body = GetComponent<Rigidbody2D>();        
+        body = GetComponent<Rigidbody2D>();
+        jumping = false;        
     }
 
     void Update()
     {
-        if (Input.GetKey("space"))
-            print("space key was pressed");
+        if (Input.GetKey("space")) jumping = true;
+        else jumping = false;        
     }
 
     void FixedUpdate()
     {
-        body.AddForce(Vector2.right * speed * Time.deltaTime);
+        if (body.velocity.x < maxVelocity) body.AddForce(Vector2.right * speed * Time.deltaTime);
+        if (jumping) body.AddForce(Vector2.up * jumpForce * Time.deltaTime);        
     }
 }
